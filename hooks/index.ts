101e3bbec1efd4034ae43e6429b905a075a31de1
Yuriy <yrippert@gmaile.com>
2026-02-10 16:46:51 +0300

    fix: re-export usePermissions properly from hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,7 @@
-export function usePermissions() {
-  return { canAccess: true, role: 'admin' as const };
-}
+export { usePermissions } from './usePermissions';
+export { useDebounce } from './useDebounce';
+export { useLocalStorage } from './useLocalStorage';
+export { usePagination } from './usePagination';
+export { useAuditTrail } from './useAuditTrail';
+export { useApiQuery } from './useApiQuery';
+export { useApiMutation } from './useApiMutation';
